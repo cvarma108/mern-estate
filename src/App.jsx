a50575329d96cf9c2b4b6464c494d6d15a4250e5
Chiranjeevi Varma Bhupathiraju <chiranjeevi.bhupathiraju@ispace.com>
2024-01-09 12:27:36 +0530

    create pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
-    <div className="text-red-500 text-xl">
-      Hello World
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/sing-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
+    
   )
 }
 
